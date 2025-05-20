3192fffe11ee82fd5ebe542b6d4115651a53349b
Darius <dariuscorvus@pm.me>
2025-05-20 13:50:18 +0200

    feat: merged lsp configs into lsp.lua

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,29 @@
+return {
+  {
+    'neovim/nvim-lspconfig',
+    opts = {
+      servers = {
+        qmlls = {
+          cmd = { 'qmlls' },
+          filetypes = { 'qml', 'qmljs' },
+          root_dir = require('lspconfig.util').root_pattern('.qmlproject', '.git'),
+        },
+        vtsls = {
+          settings = {
+            vtsls = {
+              tsserver = {
+                globalPlugins = {
+                  {
+                    enableForWorkspaceTypeScriptVersions = false,
+                    location = vim.env.TS_SVELTE_PLUGIN_PATH or '/dev/null',
+                    name = 'typescript-svelte-plugin',
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+}
