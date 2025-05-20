9fc6abf61bced20d6c1833ad8b9d079cbad02034
Darius <dariuscorvus@pm.me>
2025-05-20 13:05:27 +0200

    feat: using node2nix

--- overlays/typescript-svelte-plugin.nix
+++ overlays/typescript-svelte-plugin.nix
@@ -1,20 +1,6 @@
 self: super: {
-  typescript-svelte-plugin = super.buildNpmPackage {
-    pname = "typescript-svelte-plugin";
-    version = "0.3.47";
-
-    src = super.fetchurl {
-      url = "https://registry.npmjs.org/typescript-svelte-plugin/-/typescript-svelte-plugin-0.3.47.tgz";
-      hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
-    };
-
-    npmDepsHash = "sha256-BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB=";
-
-    dontNpmBuild = true;
-
-    installPhase = ''
-      mkdir -p $out/lib/node_modules/typescript-svelte-plugin
-      cp -r . $out/lib/node_modules/typescript-svelte-plugin
-    '';
+  typescript-svelte-plugin = import ../nix/node-packages/typescript-svelte-plugin-composition.nix {
+    inherit (super) pkgs system;
+    nodejs = super.nodejs_24;
   };
 }
