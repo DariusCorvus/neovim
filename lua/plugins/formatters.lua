46a2ab072b041ba9c15bc824de16212714397459
Darius <dariuscorvus@pm.me>
2025-06-04 12:48:00 +0200

    feat: added linter and formatter for sql

--- lua/plugins/formatters.lua
+++ lua/plugins/formatters.lua
@@ -3,8 +3,11 @@ return {
     'stevearc/conform.nvim',
     optional = true,
     opts = function(_, opts)
+      local sql_ft = { 'sql' }
       opts.formatters.sqlfluff = {
         args = { 'format', '--dialect=ansi', '-' },
+        stdin = true,
+        require_cwd = false,
       }
       for _, ft in ipairs(sql_ft) do
         opts.formatters_by_ft[ft] = opts.formatters_by_ft[ft] or {}
--- lua/plugins/linters.lua
+++ lua/plugins/linters.lua
@@ -3,6 +3,7 @@ return {
     'mfussenegger/nvim-lint',
     optional = true,
     opts = function(_, opts)
+      local sql_ft = { 'sql' }
       for _, ft in ipairs(sql_ft) do
         opts.linters_by_ft[ft] = opts.linters_by_ft[ft] or {}
         table.insert(opts.linters_by_ft[ft], 'sqlfluff')
