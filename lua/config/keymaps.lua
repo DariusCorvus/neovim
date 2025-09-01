1991dcc4c95feca73fd5d2138ab3166be2d98a10
darius.codes <commits@darius.codes>
2025-09-01 04:55:35 +0200

    feat: removed persisted

--- lua/config/keymaps.lua
+++
@@ -1,3 +0,0 @@
-vim.api.nvim_set_keymap('n', '<MiddleMouse>', '<Nop>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('i', '<MiddleMouse>', '<Nop>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('v', '<MiddleMouse>', '<Nop>', { noremap = true, silent = true })
